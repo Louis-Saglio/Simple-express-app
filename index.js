f39ebdec9f741e47bfaad09dee6caabb1d69545f
Louis-Saglio <louis.saglio@sfr.fr>
2018-06-07 14:55:21 +0200

    [clean] Add ;

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const db = require('sqlite')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const db = require('sqlite');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // DATABASE
 db.open('expressapi.db').then(() => {
@@ -12,27 +12,27 @@ db.open('expressapi.db').then(() => {
     .then(() => {
       console.log('> Database ready')
     }).catch((err) => { // Si on a eu des erreurs
-      console.error('ERR> ', err)
-    })
-})
+    console.error('ERR> ', err)
+  })
+});
 
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 // BODY PARSER
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
 // Override POST
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 // LOGGER
 app.use((req, res, next) => {
-  next()
+  next();
   console.log('REQUEST: ' + req.method + ' ' + req.url)
-})
+});
 
 // DEFAULT ROUTE
 app.get('/', (req, res, next) => {
@@ -40,11 +40,11 @@ app.get('/', (req, res, next) => {
     html: () => { res.send('<h1>Bienvenue sur notre superbe API!</h1>') },
     json: () => { res.send({ message: 'Bienvenue sur notre superbe API!' }) }
   })
-})
+});
 
 // GET ALL USERS
-app.get('/users', (req, res, next) => {  
-  const wheres = []
+app.get('/users', (req, res, next) => {
+  const wheres = [];
 
   if (req.query.firstname) {
     wheres.push(`firstname LIKE '%${req.query.firstname}%'`)
@@ -54,30 +54,30 @@ app.get('/users', (req, res, next) => {
     wheres.push(`lastname LIKE '%${req.query.lastname}%'`)
   }
 
-  const limit = `LIMIT ${req.query.limit || 100}`
-  const offset = `OFFSET ${ req.query.offset || 0}`
-  const where = wheres.length > 0 ? `WHERE ${wheres.join(' AND ')}` : ''
-  let order = ''
-  let reverse = ''
+  const limit = `LIMIT ${req.query.limit || 100}`;
+  const offset = `OFFSET ${ req.query.offset || 0}`;
+  const where = wheres.length > 0 ? `WHERE ${wheres.join(' AND ')}` : '';
+  let order = '';
+  let reverse = '';
   if (req.query.order && req.query.reverse) {
-    order = `ORDER BY ${req.query.order}`
-    if (req.query.reverse == '1') {
+    order = `ORDER BY ${req.query.order}`;
+    if (req.query.reverse === '1') {
       reverse = 'DESC'
-    } else if (req.query.reverse == '0') {
+    } else if (req.query.reverse === '0') {
       reverse = 'ASC'
     }
   }
-  
-  query = `SELECT * FROM users ${where} ${order} ${reverse} ${limit} ${offset}`
+
+  query = `SELECT * FROM users ${where} ${order} ${reverse} ${limit} ${offset}`;
 
   db.all(query)
-  .then((users) => {
-    res.format({
-      html: () => { res.render('users/index', { users: users }) },
-      json: () => { res.send(users) }
-    })
-  }).catch(next)
-})
+    .then((users) => {
+      res.format({
+        html: () => { res.render('users/index', { users: users }) },
+        json: () => { res.send(users) }
+      })
+    }).catch(next)
+});
 
 // VIEW: ADD USER
 app.get('/users/add', (req, res, next) => {
@@ -93,7 +93,7 @@ app.get('/users/add', (req, res, next) => {
       next(new Error('Bad request'))
     }
   })
-})
+});
 
 // VIEW: EDIT USER
 app.get('/users/:userId/edit', (req, res, next) => {
@@ -113,7 +113,7 @@ app.get('/users/:userId/edit', (req, res, next) => {
       next(new Error('Bad request'))
     }
   })
-})
+});
 
 // GET USER BY ID
 app.get('/users/:userId', (req, res, next) => {
@@ -132,41 +132,49 @@ app.post('/users', (req, res, next) => {
     next(new Error('All fields must be given.'))
   }
 
-  db.run("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)", req.body.pseudo, req.body.email, req.body.firstname, req.body.lastname, new Date(), null)
-  .then(() => {
-    res.format({
-      html: () => { res.redirect('/users') },
-      json: () => { res.status(201).send({message: 'success'}) }
-    })
-  }).catch(next)
-})
+  db.run(
+    "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
+    req.body.pseudo,
+    req.body.email,
+    req.body.firstname,
+    req.body.lastname,
+    new Date(),
+    null
+  )
+    .then(() => {
+      res.format({
+        html: () => { res.redirect('/users') },
+        json: () => { res.status(201).send({message: 'success'}) }
+      })
+    }).catch(next)
+});
 
 // DELETE USER
 app.delete('/users/:userId', (req, res, next) => {
   db.run('DELETE FROM users WHERE ROWID = ?', req.params.userId)
-  .then(() => {
-    res.format({
-      html: () => { res.redirect('/users') },
-      json: () => { res.status(201).send({message: 'success'}) }
-    })
-  }).catch(next)
-})
+    .then(() => {
+      res.format({
+        html: () => { res.redirect('/users') },
+        json: () => { res.status(201).send({message: 'success'}) }
+      })
+    }).catch(next)
+});
 
 // UPDATE USER
-app.put('/users/:userId', (req, res, next) => { 
+app.put('/users/:userId', (req, res, next) => {
   db.run("UPDATE users SET pseudo = ?, email = ?, firstname = ?, lastname = ?, updatedAt= ? WHERE rowid = ?",req.body.pseudo, req.body.email, req.body.firstname, req.body.lastname, new Date(), req.params.userId)
-  .then(() => {
-    res.format({
-      html: () => { res.redirect('/users') },
-      json: () => { res.status(201).send({message: 'success'}) }
-    })
-  }).catch(next)
-})
+    .then(() => {
+      res.format({
+        html: () => { res.redirect('/users') },
+        json: () => { res.status(201).send({message: 'success'}) }
+      })
+    }).catch(next)
+});
 
 // ERROR
 app.use((err, req, res, next) => {
-  console.log('ERR: ' + err)
-  res.status(500)
+  console.log('ERR: ' + err);
+  res.status(500);
   res.format({
     html: () => {
       res.end('Server Error')
@@ -175,7 +183,7 @@ app.use((err, req, res, next) => {
       res.send({status: 500, message: err})
     }
   })
-})
+});
 
 // 501
 app.use((req, res) => {
@@ -184,12 +192,12 @@ app.use((req, res) => {
       res.render('501')
     },
     json: () => {
-      res.status(501)
+      res.status(501);
       res.send({status: 501, message: 'Not implemented'})
     }
   })
-})
+});
 
 app.listen(PORT, () => {
   console.log('Server running on port: ' + PORT)
-})
+});
